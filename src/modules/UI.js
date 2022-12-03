cf83ddabd3c3ecf5e47abd4c1b6f66379a3fb7f5
Jarrell Mikko Romero <jarrellmikkoromero@gmail.com>
2022-12-03 22:44:09 +0800

    Creating tests for fetching data

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -2,7 +2,29 @@ import weatherModule from './weather';
 
 const UI = (() => {
   const loadApp = () => {
-    console.log(weatherModule.getData('London'));
+    // console.log(weatherModule.getData('London'));
+    const searchBtn = document.querySelector('.search-btn');
+
+    searchBtn.addEventListener('click', loadLocationWeather);
+  };
+
+  const loadLocationWeather = async () => {
+    const location = document.querySelector('.search-bar').value.trim();
+    if (location === '') {
+      alert('Location cant be empty');
+      return;
+    }
+    const cityName = await weatherModule.getCityName(location);
+    console.log(cityName);
+    // displayName(cityName);
+  };
+
+  const displayName = (cityName) => {
+    const main = document.querySelector('main');
+    const div = document.createElement('div');
+
+    div.textContent = cityName;
+    main.appendChild(div);
   };
 
   return { loadApp };
--- src/modules/weather.js
+++ src/modules/weather.js
@@ -1,6 +1,16 @@
 const weatherModule = (() => {
   const apiKey = 'ccaa29ffc6e74301d0c01334f31ddd78';
 
+  const getCityName = async (cityName) => {
+    try {
+      const data = await getData(cityName);
+
+      return data.name;
+    } catch (error) {
+      return error;
+    }
+  };
+
   const getData = async (cityName) => {
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
     try {
@@ -16,7 +26,7 @@ const weatherModule = (() => {
 
   const getWeather = () => getData().weather;
 
-  return { getData, getWeather };
+  return { getData, getWeather, getCityName };
 })();
 
 export default weatherModule;
