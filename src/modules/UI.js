6fe6b39b2262a94dcbd5617cf599dc5a56d93cf3
Jarrell Mikko Romero <jarrellmikkoromero@gmail.com>
2022-12-05 17:14:23 +0800

    Fix bugs and clean code

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -2,40 +2,48 @@ import weatherModule from './weather';
 
 const UI = (() => {
   const loadApp = () => {
-    const searchBar = document.querySelector('.search-bar');
-    const searchBtn = document.querySelector('.search-btn');
-
-    searchBar.addEventListener('keypress', (e) => {
-      if (e.key === 'Enter') {
-        searchBtn.click();
-      }
-    });
-    searchBtn.addEventListener('click', loadLocationWeather);
     displayWeatherInfo('Mabalacat', 'imperial');
+    initButtons();
   };
 
-  const loadLocationWeather = async () => {
-    const searchBar = document.querySelector('.search-bar');
+  const loadLocationWeather = (e) => {
     const searchBarValue = document.querySelector('.search-bar').value.trim();
-    const data = await weatherModule.getData(searchBarValue);
+    const cityName = document.querySelector('.city-name').textContent;
+    const metricBtn = document.querySelector('.change-unit-metric');
+    const imperialBtn = document.querySelector('.change-unit-imperial');
 
-    if (searchBarValue === '') {
-      alert('Location cant be empty');
-      return;
+    if (e.target === metricBtn) {
+      imperialBtn.classList.remove('active');
+      metricBtn.classList.add('active');
+      displayWeatherInfo(cityName, 'metric');
+    } else if (e.target === imperialBtn) {
+      metricBtn.classList.remove('active');
+      imperialBtn.classList.add('active');
+      displayWeatherInfo(cityName, 'imperial');
+    } else {
+      if (searchBarValue === '') {
+        alert(`Location can't be empty`);
+        return;
+      }
+
+      if (metricBtn.classList.contains('active')) {
+        displayWeatherInfo(searchBarValue, 'metric');
+        return;
+      }
+      displayWeatherInfo(searchBarValue, 'imperial');
     }
-
-    if (data.name === undefined) {
-      alert('City not found');
-      searchBar.value = '';
-      return;
-    }
-
-    displayWeatherInfo(searchBarValue, 'imperial');
-    searchBar.value = '';
   };
 
   const displayWeatherInfo = async (city, unit) => {
     const data = await weatherModule.getData(city, unit);
+    const searchBar = document.querySelector('.search-bar');
+
+    if (data.name === undefined) {
+      alert(`City ${searchBar.value} not found`);
+      searchBar.value = '';
+      return;
+    }
+
     const cityName = data.name;
     const weatherIcon = data.weather[0].icon;
     const weatherDescription = data.weather[0].description;
@@ -48,54 +56,48 @@ const UI = (() => {
     if (unit === 'metric') {
       container.innerHTML = `
       <div class="left-panel">
-        <img src="../dist/icons/${weatherIcon}.png" class="weather-icon">
+        <img src="./icons/${weatherIcon}.png" class="weather-icon">
       </div>
-      <div class="mid-panel">
+      <div class="right-panel">
         <span class="weather-description">${weatherDescription}</span>
         <span class="city-name">${cityName}</span>
         <span class="temperature">Feels like: ${temperature} &#8451;</span>
         <span class="humidity">Humidity: ${humidity}%</span>
         <span class="wind-speed">Wind Speed: ${windSpeed} km/h</span>
       </div>
-      <div class="right-panel">
-        <button class="change-unit-metric active">Metric</button>
-        <button class="change-unit-imperial">Imperial</button>
-      </div>
     `;
     } else if (unit === 'imperial') {
       container.innerHTML = `
         <div class="left-panel">
-          <img src="../dist/icons/${weatherIcon}.png" class="weather-icon">
+          <img src="./icons/${weatherIcon}.png" class="weather-icon">
         </div>
-        <div class="mid-panel">
+        <div class="right-panel">
           <span class="weather-description">${weatherDescription}</span>
           <span class="city-name">${cityName}</span>
           <span class="temperature">Feels like: ${temperature} &#8457;</span>
           <span class="humidity">Humidity: ${humidity}%</span>
           <span class="wind-speed">Wind Speed: ${windSpeed} mph</span>
         </div>
-        <div class="right-panel">
-          <button class="change-unit-metric">Metric</button>
-          <button class="change-unit-imperial active">Imperial</button>
-        </div>
       `;
     }
-
-    initUnitButtons();
   };
 
-  const initUnitButtons = () => {
-    const cityName = document.querySelector('.city-name').textContent;
+  const initButtons = () => {
+    const searchBar = document.querySelector('.search-bar');
+    const searchBtn = document.querySelector('.search-btn');
     const metricBtn = document.querySelector('.change-unit-metric');
     const imperialBtn = document.querySelector('.change-unit-imperial');
 
-    metricBtn.addEventListener('click', () => {
-      displayWeatherInfo(cityName, 'metric');
+    searchBar.addEventListener('keypress', (e) => {
+      if (e.key === 'Enter') {
+        searchBtn.click();
+      }
     });
+    searchBtn.addEventListener('click', loadLocationWeather);
 
-    imperialBtn.addEventListener('click', () => {
-      displayWeatherInfo(cityName, 'imperial');
-    });
+    metricBtn.addEventListener('click', loadLocationWeather);
+
+    imperialBtn.addEventListener('click', loadLocationWeather);
   };
 
   return { loadApp };
